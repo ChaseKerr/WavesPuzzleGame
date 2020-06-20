ae4c6ca1b948d626b7e6e3bafc3fe5c47fbc2141
ChaseKerr <chasekerr18@yahoo.com>
2020-06-19 20:11:31 -0400

    Prototyped wave creation (possible memory leak as fps decreases over time), and added UserInput script.

---
+++ Waves Puzzle Game/Assets/Scripts/UserInput.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    // PRIVATE
+    private float tapTimeStamp = 0.0f;  // time at which the user clicked or tapped the screen (Time.time)
+    private bool userHolding = false;   // true if the player is currently holding on the screen (opposed to a tap or click)
+
+    // OBJECTS
+    //[SerializeField]
+    //private BlocksController blockController; // blocks controller script
+
+    // PRIVATE INSPECTOR
+    [SerializeField]
+    private float minHoldTime = 0.0f;           // minimum amount of time for a click to become a hold
+    
+    // BASIC UNITY
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (Input.touchCount > 0) // at least 1 touch
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                // touch started
+                case TouchPhase.Began:
+
+                    tapTimeStamp = Time.time; // record time of initial tap
+
+                    // not inspecting block (check if one was tapped on)
+                    if (!blockController.IsInspectingBlock())
+                    {
+                        RaycastHit hit;
+                        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+
+                        if (Physics.Raycast(ray, out hit))
+                        {
+                            if (hit.transform.tag == "Block")
+                            {
+                                blockController.SetAsActiveBlock(hit.transform);
+                                print(hit.transform.name); // OMIT
+                            }
+                        }
+                    }
+                    break;
+
+                // hold / touch not moving
+                case TouchPhase.Stationary:
+                    
+                    if (!userHolding && Time.time - tapTimeStamp >= minHoldTime)
+                    {
+                        userHolding = true;
+                    }
+                    
+                    break;
+
+                // hold / touch is moving
+                case TouchPhase.Moved:
+                    if (!userHolding)
+                    {
+                        userHolding = true;
+                    }
+
+                    // inpsecting block
+                    if (blockController.IsInspectingBlock())
+                    {
+                        blockController.RotateInspectBlock(-touch.deltaPosition.x);
+                    }
+                    break;
+                
+                // touch ended
+                case TouchPhase.Ended:
+
+                    // user held
+                    if (userHolding)
+                    {
+                        // user was moving block
+                        if (blockController.IsMovingBlock())
+                        {
+                            blockController.DropBlock();
+                        }
+                    }
+                    // user tapped
+                    else
+                    {
+                        // user was inspecting block
+                        if (blockController.IsInspectingBlock())
+                        {
+                            blockController.StopInspectBlock();
+                        }
+
+                        // user tapped on a block
+                        else if (blockController.ABlockIsActive())
+                        {
+                            // inspect block
+                            if (!blockController.ActiveBlockOnGrid()) // block isn't placed on grid
+                            {
+                                blockController.InspectBlock();
+                            }
+                            // rotate placed block
+                            else // block is on grid
+                            {
+                                Vector3 rotation = new Vector3(0, 90, 0);
+                                blockController.RotateBlock(rotation);
+                            }
+                        }
+                    }
+                    // reset variables
+                    tapTimeStamp = 0.0f;
+                    userHolding = false;
+
+                    break;
+            }
+        }
+        */
+    }
+
+    // PUBLIC
+    public bool UserIsHolding()
+    {
+        return userHolding;
+    }
+}
--- Waves Puzzle Game/Assets/Scripts/WaterMeshGenerator.cs
+++ Waves Puzzle Game/Assets/Scripts/WaterMeshGenerator.cs
@@ -16,6 +16,11 @@ public class WaterMeshGenerator : MonoBehaviour
     [SerializeField]
     private int ySize = 0;
 
+    // temporary
+    private List<bool> activated = new List<bool>();  // list of which indices are active
+    private List<float> times = new List<float>();
+    private float waveRadius = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,18 +37,73 @@ public class WaterMeshGenerator : MonoBehaviour
         UpdateMesh();
 
         mesh.RecalculateNormals();
+
+        // temp
+        for (int i = 0; i < xSize * ySize; ++i)
+        {
+            activated.Add(false);
+            times.Add(0.0f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // update vertices
-        for (int i = 0; i < 10; ++i)
-            vertices[Random.Range(0, xSize * ySize)].y += 0.25f;
+        waveRadius += 5 * Time.deltaTime;
+
+        // UPDATE VERTICES
+
+        // calculate next vertices to be activated
+        Vector2Int origin = new Vector2Int(50, 50);
+        List<Vector2Int> next = new List<Vector2Int>();
+        float threshold = waveRadius * waveRadius;
+
+        for (int i = (int)-waveRadius; i <= (int)waveRadius; ++i)
+        {
+            for (int j = (int)-waveRadius; j <= (int)waveRadius; ++j)
+            {
+                if (i * i + j * j < threshold)
+                {
+                    next.Add(new Vector2Int(i, j));
+                }
+            }
+        }
+
+        // check all points and activate points not already active
+        foreach (Vector2Int pos in next)
+        {
+            // original pos centered around 0,0
+            // so this adjusts by shifting the wave origin
+            Vector2Int shifted = pos + origin;
+
+            if (shifted.x >= 0 && shifted.x < xSize &&
+                shifted.y >= 0 && shifted.y < ySize)
+            {
+                // convert pos to index
+                int index = GetNearestWaterVertexIndex(shifted);
+
+                // add index if not already activated
+                if (!activated[index])
+                {
+                    activated[index] = true;
+                    times[index] = Time.time;
+                }
+            }
+        }
+
+        // update all vertices
+        for (int v = 0; v < xSize * ySize; ++v)
+        {
+            if (activated[v])
+            {
+                vertices[v].y = Mathf.Sin(Time.time - times[v]);
+            }
+        }
 
         UpdateMesh();
     }
 
+    // Creates the mesh representing the water
     void CreateWater()
     {
         // vertices
@@ -81,6 +141,7 @@ public class WaterMeshGenerator : MonoBehaviour
         }
     }
 
+    // Updates the mesh's vertices, triangles, and recalculates normals (costly)
     void UpdateMesh()
     {
         mesh.Clear();
@@ -89,5 +150,26 @@ public class WaterMeshGenerator : MonoBehaviour
         mesh.triangles = triangles;
 
         mesh.RecalculateNormals();
+    }
+
+    // Given a position in world space, converts to local space (of water) 
+    // and returns nearest vertex index (of vertices array)
+    int GetNearestWaterVertexIndex(Vector3 position)
+    {
+        Vector3Int roundedPos = Vector3Int.RoundToInt(position);
+        return (roundedPos.z * ySize) + roundedPos.x;
+    }
+
+    int GetNearestWaterVertexIndex(Vector2Int position)
+    {
+        return (position.y * ySize) + position.x;
+    }
+
+    // PUBLIC
+    public void CreateWave(Vector3 rawOrigin)
+    {
+        int waveOrigin = GetNearestWaterVertexIndex(rawOrigin);
+
+        // more
     }
 }
